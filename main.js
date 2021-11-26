0d9488c6712aff1d8c1630c0378b33c8e16dfe48
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-26 07:11:48 -0800

    nechta a nechta s borligi

---
+++ main.js
@@ -0,0 +1,37 @@
+const title = [
+    {
+        id:1,
+        name:'Asadaaa',
+        status:'Students',
+    }
+
+];
+
+a =0;
+s = 0;
+for (let i = 0; i < title.length; i++) {
+NewNAme = title[0].name;
+
+NewStatus = title[0].status;
+
+
+for (let j = 0; j < NewNAme.length; j++) {
+
+    (NewNAme[j] === 'a' || NewNAme[j] === 'A') && a++;
+
+
+
+}
+
+for (let k = 0; k < NewStatus.length; k++) {
+
+
+    (NewStatus[k] === 's' || NewStatus[k] === 'S') && s++;
+
+
+}
+
+}
+
+console.log(a,'=a');
+console.log(s,'=s');
